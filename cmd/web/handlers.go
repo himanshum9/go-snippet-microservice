f6cdc536513c92b89322d40e5284c4a4d5ed3c71
himanshum9 <contact.himanshum9@gmail.com>
2024-05-06 20:10:13 +0530

    added helper file for commom thingd

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	files := []string{
@@ -22,13 +22,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		// Because the home handler function is now a method against application
 		// it can access its fields, including the error logger. We'll write the log
 		// message to this instead of the standard logger.
-		app.errorlog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		// app.errorlog.Println(err.Error())
+		app.serverError(w, err)
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorlog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		// app.errorlog.Println(err.Error())
+		app.serverError(w, err)
+		// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
@@ -37,7 +38,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
+		// http.NotFound(w, r)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
@@ -45,7 +47,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		// http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
